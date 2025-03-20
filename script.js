0c554395dbbb8819d388cc7172a148baef06704f
Valetina-2608 <valusha2608@gmail.com>
2025-03-20 21:24:20 +0200

    Add funcion addNewTask

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 /* script.js */
+
+let add_task=document.getElementById('add_task');
+let new_task=document.querySelector('.new_task');
+
+add_task.addEventListener('click', addNewTask);
+
+function addNewTask(){
+    new_task.style.display='block';
+}
